dd069b2ecdc193b6c0d6a3b98438ba6ef5e10827
Joelad <joeladdarwin@gmail.com>
2018-09-20 11:04:22 +0530

    Register changes made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { environment } from "../environments/environment";
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from "angularfire2/auth";
 import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { MatBadgeModule } from '@angular/material/badge';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './client/navbar/navbar.component';
@@ -43,6 +44,7 @@ import { AdminService } from './shared/admin.service';
 import { AgentService } from './shared/agent.service';
 import { RegisterComponent } from './client/register/register.component';
 import { ForgetComponent } from './client/forget/forget.component';
+import { FooterComponent } from './client/footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -67,15 +69,15 @@ import { ForgetComponent } from './client/forget/forget.component';
     AgentloginComponent,
     LoginComponent,
     RegisterComponent,
-    ForgetComponent
+    ForgetComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     CommonModule,
     AppRoutingModule,
-
-  
+    MatBadgeModule,  
     FormsModule,
     AngularFireAuthModule, AngularFirestoreModule,
     AngularFireModule.initializeApp(environment.firebase),
--- src/app/client/dashboard/dashboard.component.ts
+++ src/app/client/dashboard/dashboard.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ClientService } from '../../shared/client.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'], 
+  providers:[ClientService]
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+name:any;
+  constructor(private cli:ClientService) { }
 
   ngOnInit() {
+this.name = this.cli.getuser();
+console.log(this.name);
   }
 
 }
