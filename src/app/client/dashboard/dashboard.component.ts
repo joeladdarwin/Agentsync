f1f7214e51b9ba34587ddbdf7b8ab00cb4de9408
gayathri <gayathrijayabalan96@gmail.com>
2018-09-28 10:46:48 +0530

    placeorder images replaced by gayathri

--- src/app/client/dashboard/dashboard.component.ts
+++ src/app/client/dashboard/dashboard.component.ts
@@ -8,12 +8,12 @@ import { AuthService } from '../../shared/auth.service';
   providers:[ClientService, AuthService]
 })
 export class DashboardComponent implements OnInit {
-// name:any;
-// message:any;
-//   constructor(private cli:ClientService,  private auth:AuthService) {
-//     this.name=this.cli.getUsername();
+name:any;
+message:any;
+  constructor(private cli:ClientService,  private auth:AuthService) {
+    this.name=this.cli.getUsername();
   
-//    }
+   }
 
   ngOnInit() {
     this.name = this.auth.getinfox();
