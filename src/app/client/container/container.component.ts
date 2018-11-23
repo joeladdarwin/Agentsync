e5ec37eeff4d7823241df66cb01b75cabebcf5f0
AlexSoga <alexpandiyan15895@gmail.com>
2018-11-23 15:19:57 +0530

    profile

--- src/app/client/container/container.component.ts
+++ src/app/client/container/container.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../shared/auth.service';
 
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
   styleUrls: ['./container.component.css']
 })
-export class ContainerComponent implements OnInit {
+export class ContainerComponent {
   showFiller = false;
-  constructor() { }
+  profile:any;
+  constructor(private auth:AuthService) { }
 
   ngOnInit() {
+    this.profile = this.auth.users$;
+
+  }
+  logout(){
+    this.auth.signOut;
   }
 
 }
