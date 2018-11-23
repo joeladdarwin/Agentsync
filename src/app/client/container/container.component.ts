76f76edb802d961c4d08ad8e11fc14a2fff35952
AlexSoga <alexpandiyan15895@gmail.com>
2018-11-23 17:53:22 +0530

    alex changes

--- src/app/client/container/container.component.ts
+++ src/app/client/container/container.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../shared/auth.service';
-
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFirestore,QuerySnapshot } from 'angularfire2/firestore';
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
@@ -9,10 +10,15 @@ import { AuthService } from '../../shared/auth.service';
 export class ContainerComponent {
   showFiller = false;
   profile:any;
-  constructor(private auth:AuthService) { }
+  userid:any;
+  photo:any;
+  constructor(private auth:AuthService,public afauth:AngularFireAuth,private afs: AngularFirestore) { }
 
   ngOnInit() {
     this.profile = this.auth.users$;
+    this.userid = this.afauth.auth.currentUser.uid;
+   this.photo=this.afs.collection<any>('users').doc(this.userid).valueChanges();
+    
 
   }
   logout(){
--- src/app/client/profile/profile.component.ts
+++ src/app/client/profile/profile.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { ClientService } from '../../shared/client.service';
 import { AuthService } from '../../shared/auth.service';
 import { AngularFireAuth } from 'angularfire2/auth';
-
+import { AngularFirestore,QuerySnapshot } from 'angularfire2/firestore';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -19,8 +19,9 @@ export class ProfileComponent implements OnInit, AfterViewInit {
   pro:any;
   title;
   user;
+  photo:any;
   userid:any;
-  constructor(private cli: ClientService, public auth: AuthService, public afauth:AngularFireAuth) { }
+  constructor(private cli: ClientService, public auth: AuthService, public afauth:AngularFireAuth,private afs: AngularFirestore) { }
   ngAfterViewInit(){
     this.name = this.auth.displayName;
     this.email = this.auth.email;
@@ -28,6 +29,8 @@ export class ProfileComponent implements OnInit, AfterViewInit {
     this.userid = this.afauth.auth.currentUser.uid; 
     console.log("darwin"+this.userid);
     console.log(this.user + "is the uid")
+
+
   }
   gotoeditprofile()
   {
@@ -37,6 +40,9 @@ export class ProfileComponent implements OnInit, AfterViewInit {
 
     this.auth.users$.subscribe(user=>this.user =user)
  console.log(this.user.uid);
+ this.userid = this.afauth.auth.currentUser.uid;
+ this.photo=this.afs.collection<any>('users').doc(this.userid).valueChanges();
+    
     
   }
 
